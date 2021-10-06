cbefbb55a6998f27524f78495e2778126e5765b0
faudzanfadhli <faudzan.fadhli.of.pd.m.fc@gmail.com>
2021-10-06 22:00:07 +0700

    Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+  <h1><?php echo $judul ?></h1>
+  <h4>Nama</h4>
+  <ul type="disc">
+    <li>Nama Depan: Fauzan</li>
+    <li>Nama Belakang: Fadli</li>
+  </ul>
+  <br>
+  <h4>Alamat</h4>
+  <ul type="none">
+    <li>Jatimulya, Tambun Selatan, Bekasi</li>
+  </ul>
+  <h4>Tempat Lahir</h4>
+  <ul type="none">
+    <li>Bekasi</li>
+  </ul>
+  <h4>Olahraga Favorit</h4>
+  <ul type="square">
+    <li>Lari</li>
+    <li>Catur</li>
+  </ul>
+</section>
